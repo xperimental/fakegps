f8b52c7549ef12999d3379be292eadced6f55da6
Robert <xperimental@solidproject.de>
2010-02-16 18:31:00 +0100

    Added JavaDoc.

--- src/main/java/net/sourcewalker/fakegps/gui/WaypointActions.java
+++ src/main/java/net/sourcewalker/fakegps/gui/WaypointActions.java
@@ -9,20 +9,51 @@ import net.sourcewalker.fakegps.data.MapTool;
 
 import org.jdesktop.application.Action;
 
+/**
+ * This class contains action definitions connected to managing the waypoints of
+ * the route. The actions are used within the {@link WaypointPanel} GUI element.
+ * 
+ * @author Xperimental
+ */
 public class WaypointActions {
 
+    /**
+     * Contains a reference to the data model which is modified by the actions.
+     */
     private IDataModel model;
 
-    public WaypointActions(IDataModel dataModel) {
+    /**
+     * Create a new instance using the data model provided.
+     * 
+     * @param dataModel
+     *            Data model to modify.
+     */
+    public WaypointActions(final IDataModel dataModel) {
         this.model = dataModel;
     }
 
+    /**
+     * Activates the map tool for adding points to the route.
+     * 
+     * @param evt
+     *            Event with a {@link JToggleButton} source to indicate whether
+     *            the tool should be set active.
+     */
     @Action
-    public void addPoint(ActionEvent evt) {
+    public final void addPoint(final ActionEvent evt) {
         toggleMapTool(evt, MapTool.ADDPOINT);
     }
 
-    private void toggleMapTool(ActionEvent evt, MapTool tool) {
+    /**
+     * Toggle a map tool.
+     * 
+     * @param evt
+     *            Event with a {@link JToggleButton} source to indicate whether
+     *            the tool should be set active.
+     * @param tool
+     *            Tool to toggle active or inactive.
+     */
+    private void toggleMapTool(final ActionEvent evt, final MapTool tool) {
         JToggleButton source = (JToggleButton) evt.getSource();
         if (source.isSelected()) {
             model.setCurrentTool(tool);
@@ -31,13 +62,23 @@ public class WaypointActions {
         }
     }
 
+    /**
+     * Activates the map tool for removing points from the route.
+     * 
+     * @param evt
+     *            Event with a {@link JToggleButton} source to indicate whether
+     *            the tool should be set active.
+     */
     @Action
-    public void removePoint(ActionEvent evt) {
+    public final void removePoint(final ActionEvent evt) {
         toggleMapTool(evt, MapTool.REMOVEPOINT);
     }
 
+    /**
+     * Clears the currently set route.
+     */
     @Action
-    public void clearPoints() {
+    public final void clearPoints() {
         model.clearWaypoints();
     }
 
---
+++ src/main/java/net/sourcewalker/fakegps/gui/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains classes which make up the graphical interface of the application.
+ */
+package net.sourcewalker.fakegps.gui;
+
